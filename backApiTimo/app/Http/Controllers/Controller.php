b7395ca914f89a411446e55ba66f18d3666c7800
João Scaleão <j.scaleao@hotmail.com>
2021-09-21 00:57:21 -0300

    feat: Encapculando Dados no response e transformando em JSON

--- backApiTimo/app/Http/Controllers/Controller.php
+++ backApiTimo/app/Http/Controllers/Controller.php
@@ -10,4 +10,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendData($data){
+        return $this->sendMessage("", $data);
+    }
+
+
+    public function sendMessage($message, $data=null){
+        $response = [
+            "success"=> true,
+            "message" => $message,
+            "data" => $data
+        ];
+        return response()->json($response);
+    }
+
+    public function sendError($message, $status=500){
+        $response = [
+            "success"=> false,
+            "message" => $message,
+            "data" => null
+        ];
+        return response()->json($response, $status);
+    }
 }
